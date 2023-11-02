ef73b36af141e7e29d9ee57d31461f65ce7fbeba
Hanna1991-sys <119236984+Hanna1991-sys@users.noreply.github.com>
2023-11-02 23:37:07 +0200

    2 commid

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const link = document.querySelector('.gallery');
+
+const itemOfImgString = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}" 
+            />
+          </a>
+        </li>
+    `
+  )
+  .join('');
+
+link.innerHTML = itemOfImgString;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
